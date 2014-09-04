7aa32057f04095b4b0cc37e3a5c0ed6345474272
Boy Baukema <boy@ibuildings.nl>
2014-09-04 15:38:27 +0200

    Add a constructor for Service value object

--- src/Service.php
+++ src/Service.php
@@ -13,4 +13,14 @@ class Service
      * @var string
      */
     public $location;
+
+    /**
+     * @param string $location
+     * @param string $binding
+     */
+    public function __construct($location, $binding)
+    {
+        $this->binding  = $binding;
+        $this->location = $location;
+    }
 }
