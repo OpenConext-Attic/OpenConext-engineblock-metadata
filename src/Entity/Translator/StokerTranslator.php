f7aba6d05162f089aac02b474709739a0521e1fa
Boy Baukema <boy@ibuildings.nl>
2014-09-15 21:49:43 +0200

    Fixed namspaces

--- src/Entity/Translator/StokerTranslator.php
+++ src/Entity/Translator/StokerTranslator.php
@@ -2,6 +2,7 @@
 
 namespace OpenConext\Component\EngineBlockMetadata\Entity\Translator;
 
+use DOMDocument;
 use OpenConext\Component\EngineBlockMetadata\Entity\AbstractConfigurationEntity;
 use OpenConext\Component\EngineBlockMetadata\Entity\IdentityProviderEntity;
 use OpenConext\Component\EngineBlockMetadata\Entity\ServiceProviderEntity;
@@ -9,41 +10,49 @@ use OpenConext\Component\EngineBlockMetadata\IndexedService;
 use OpenConext\Component\EngineBlockMetadata\Logo;
 use OpenConext\Component\EngineBlockMetadata\Service;
 use OpenConext\Component\StokerMetadata\MetadataIndex;
+use RuntimeException;
+use SAML2_Const;
+use SAML2_XML_md_EntityDescriptor;
+use SAML2_XML_md_IDPSSODescriptor;
+use SAML2_XML_md_RoleDescriptor;
+use SAML2_XML_md_SPSSODescriptor;
+use SAML2_XML_mdui_Logo;
+use SAML2_XML_mdui_UIInfo;
 
 class StokerTranslator
 {
     /**
      * @param $entityXml
-     * @param Entity $metadataIndexEntity
+     * @param MetadataIndex\Entity $metadataIndexEntity
      * @return IdentityProviderEntity|ServiceProviderEntity
-     * @throws \RuntimeException
+     * @throws RuntimeException
      */
-    public function translate($entityXml, Entity $metadataIndexEntity)
+    public function translate($entityXml, MetadataIndex\Entity $metadataIndexEntity)
     {
-        $document = new \DOMDocument();
+        $document = new DOMDocument();
         $document->loadXML($entityXml);
 
-        $entityDescriptor = new \SAML2_XML_md_EntityDescriptor($document->documentElement);
+        $entityDescriptor = new SAML2_XML_md_EntityDescriptor($document->documentElement);
 
         $idpDescriptor = null;
         $spDescriptor = null;
         foreach ($entityDescriptor->RoleDescriptor as $role) {
-            if ($role instanceof \SAML2_XML_md_IDPSSODescriptor) {
+            if ($role instanceof SAML2_XML_md_IDPSSODescriptor) {
                 if ($idpDescriptor) {
-                    throw new \RuntimeException('More than 1 IDPSSODescriptor found');
+                    throw new RuntimeException('More than 1 IDPSSODescriptor found');
                 }
                 $idpDescriptor = $role;
             }
-            if ($role instanceof \SAML2_XML_md_SPSSODescriptor) {
+            if ($role instanceof SAML2_XML_md_SPSSODescriptor) {
                 if ($spDescriptor) {
-                    throw new \RuntimeException('More than 1 SPSSODescriptor found');
+                    throw new RuntimeException('More than 1 SPSSODescriptor found');
                 }
                 $spDescriptor = $role;
             }
         }
 
         if (!$idpDescriptor && !$spDescriptor) {
-            throw new \RuntimeException('Entity is neither IDP nor SP?');
+            throw new RuntimeException('Entity is neither IDP nor SP?');
         }
         if ($spDescriptor && $idpDescriptor) {
             // @todo warn: adding only the idp side!
@@ -56,19 +65,19 @@ class StokerTranslator
             return $this->translateIdentityProvider($metadataIndexEntity, $entityDescriptor, $idpDescriptor);
         }
 
-        throw new \RuntimeException('Boolean logic no longer works, assume running as part of the Heart of Gold.');
+        throw new RuntimeException('Boolean logic no longer works, assume running as part of the Heart of Gold.');
     }
 
     private function translateCommon(
-        Entity $metadataIndexEntity,
+        MetadataIndex\Entity $metadataIndexEntity,
         AbstractConfigurationEntity $entity,
-        \SAML2_XML_md_RoleDescriptor $role
+        SAML2_XML_md_RoleDescriptor $role
     ) {
         $entity->displayNameNl = $metadataIndexEntity->displayNameNl;
         $entity->displayNameEn = $metadataIndexEntity->displayNameEn;
 
         foreach ($role->Extensions as $extension) {
-            if (!$extension instanceof \SAML2_XML_mdui_UIInfo) {
+            if (!$extension instanceof SAML2_XML_mdui_UIInfo) {
                 continue;
             }
 
@@ -76,7 +85,7 @@ class StokerTranslator
                 continue;
             }
 
-            /** @var \SAML2_XML_mdui_Logo $logo */
+            /** @var SAML2_XML_mdui_Logo $logo */
             $logo = $extension->Logo[0];
             $entity->logo = new Logo($logo->url);
             $entity->logo->height = $logo->height;
@@ -87,15 +96,15 @@ class StokerTranslator
     }
 
     /**
-     * @param Entity $metadataIndexEntity
-     * @param \SAML2_XML_md_EntityDescriptor $entityDescriptor
-     * @param \SAML2_XML_md_IDPSSODescriptor $idpDescriptor
+     * @param MetadataIndex\Entity $metadataIndexEntity
+     * @param SAML2_XML_md_EntityDescriptor $entityDescriptor
+     * @param SAML2_XML_md_IDPSSODescriptor $idpDescriptor
      * @return AbstractConfigurationEntity|IdentityProviderEntity
      */
     protected function translateIdentityProvider(
-        Entity $metadataIndexEntity,
-        \SAML2_XML_md_EntityDescriptor $entityDescriptor,
-        \SAML2_XML_md_IDPSSODescriptor $idpDescriptor
+        MetadataIndex\Entity $metadataIndexEntity,
+        SAML2_XML_md_EntityDescriptor $entityDescriptor,
+        SAML2_XML_md_IDPSSODescriptor $idpDescriptor
     ) {
         $entity = new IdentityProviderEntity($entityDescriptor->entityID);
 
@@ -103,7 +112,7 @@ class StokerTranslator
 
         $singleSignOnServices = array();
         foreach ($idpDescriptor->SingleSignOnService as $ssos) {
-            if (!in_array($ssos->Binding, array(\SAML2_Const::BINDING_HTTP_POST, \SAML2_Const::BINDING_HTTP_REDIRECT))) {
+            if (!in_array($ssos->Binding, array(SAML2_Const::BINDING_HTTP_POST, SAML2_Const::BINDING_HTTP_REDIRECT))) {
                 continue;
             }
 
@@ -114,19 +123,19 @@ class StokerTranslator
     }
 
     /**
-     * @param \SAML2_XML_md_EntityDescriptor $entityDescriptor
-     * @param \SAML2_XML_md_SPSSODescriptor $spDescriptor
+     * @param SAML2_XML_md_EntityDescriptor $entityDescriptor
+     * @param SAML2_XML_md_SPSSODescriptor $spDescriptor
      * @return ServiceProviderEntity
      */
     protected function translateServiceProvider(
-        \SAML2_XML_md_EntityDescriptor $entityDescriptor,
-        \SAML2_XML_md_SPSSODescriptor $spDescriptor
+        SAML2_XML_md_EntityDescriptor $entityDescriptor,
+        SAML2_XML_md_SPSSODescriptor $spDescriptor
     ) {
         $entity = new ServiceProviderEntity($entityDescriptor->entityID);
 
         $singleSignOnServices = array();
         foreach ($spDescriptor->AssertionConsumerService as $acs) {
-            if (!in_array($acs->Binding, array(\SAML2_Const::BINDING_HTTP_POST, \SAML2_Const::BINDING_HTTP_REDIRECT))) {
+            if (!in_array($acs->Binding, array(SAML2_Const::BINDING_HTTP_POST, SAML2_Const::BINDING_HTTP_REDIRECT))) {
                 continue;
             }
 
