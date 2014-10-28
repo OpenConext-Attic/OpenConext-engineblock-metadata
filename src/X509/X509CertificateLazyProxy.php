f646a7a3ba2d903d6d87d50ff7b8cc9c30686e09
Boy Baukema <boy@ibuildings.nl>
2014-10-28 14:58:09 +0100

    Import EngineBlock_X509_* as we depend on this.

---
+++ src/X509/X509CertificateLazyProxy.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace OpenConext\Component\EngineBlockMetadata;
+
+/**
+ * Lazy Proxy for X509 Certificate.
+ * Used when parsing / validation of the certificate is meant to be deferred until use
+ * (bad idea in theory, sometimes useful in practice).
+ */
+class X509CertificateLazyProxy
+{
+    /**
+     * @var X509CertificateFactory
+     */
+    private $factory;
+
+    /**
+     * @var string
+     */
+    private $certData;
+
+    /**
+     * @var X509Certificate
+     */
+    private $certificate = null;
+
+    /**
+     * @param X509CertificateFactory $factory
+     * @param $certData
+     */
+    public function __construct(X509CertificateFactory $factory, $certData)
+    {
+        $this->factory = $factory;
+        $this->certData = $certData;
+    }
+
+    /**
+     * @param $methodName
+     * @param $methodArguments
+     * @return mixed
+     */
+    public function __call($methodName, $methodArguments)
+    {
+        if (!$this->certificate) {
+            $this->certificate = $this->factory->fromCertData($this->certData);
+        }
+
+        return call_user_func_array(array($this->certificate, $methodName), $methodArguments);
+    }
+}
