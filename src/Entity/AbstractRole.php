e6d04830623c7d4d9313971ea2b96f40b7e926df
Boy Baukema <boy@ibuildings.nl>
2014-11-28 07:45:40 +0100

    Added annotations for Doctrine and moved defaults to constructor

--- src/Entity/AbstractRole.php
+++ src/Entity/AbstractRole.php
@@ -2,6 +2,7 @@
 
 namespace OpenConext\Component\EngineBlockMetadata\Entity;
 
+use DateTime;
 use OpenConext\Component\EngineBlockMetadata\Logo;
 use OpenConext\Component\EngineBlockMetadata\Organization;
 use OpenConext\Component\EngineBlockMetadata\ContactPerson;
@@ -14,6 +15,15 @@ use SAML2_Const;
  *
  * @package OpenConext\Component\EngineBlockMetadata\Entity
  * @SuppressWarnings(PHPMD.TooManyFields)
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="sso_provider_roles")
+ * @ORM\InheritanceType("SINGLE_TABLE")
+ * @ORM\DiscriminatorColumn(name="type", type="string")
+ * @ORM\DiscriminatorMap({
+ *  "sp"  = "OpenConext\EngineBlock\Entity\ServiceProvider",
+ *  "idp" = "OpenConext\EngineBlock\Entity\IdentityProvider"
+ * })
  */
 abstract class AbstractRole
 {
@@ -22,133 +32,231 @@ abstract class AbstractRole
     const WORKFLOW_STATE_DEFAULT = self::WORKFLOW_STATE_PROD;
 
     /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
      * @var string
+     *
+     * @ORM\Column(name="entity_id", type="string")
      */
     public $entityId;
 
     /**
      * @var string
+     * @ORM\Column(name="name_nl", type="string")
      */
-    public $nameNl = '';
+    public $nameNl;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="name_en", type="string")
      */
-    public $nameEn = '';
+    public $nameEn;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="description_nl", type="string")
      */
-    public $descriptionNl = '';
+    public $descriptionNl;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="description_en", type="string")
      */
-    public $descriptionEn = '';
+    public $descriptionEn;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="display_name_nl", type="string")
      */
-    public $displayNameNl = '';
+    public $displayNameNl;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="display_name_en", type="string")
      */
-    public $displayNameEn = '';
+    public $displayNameEn;
 
     /**
      * @var Logo
+     *
+     * @ORM\Column(name="logo", type="text")
      */
-    public $logo = null;
-
-    /**
-     * @var Organization
-     */
-    public $organizationNl = null;
-
-    /**
-     * @var Organization
-     */
-    public $organizationEn = null;
+    public $logo;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="organization_nl_name",type="string")
      */
-    public $keywordsNl = '';
+    public $organizationNl;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="organization_en_name",type="string")
      */
-    public $keywordsEn = '';
+    public $organizationEn;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="keywords_nl", type="string")
+     */
+    public $keywordsNl;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="keywords_en", type="string")
+     */
+    public $keywordsEn;
 
     /**
      * @var bool
+     *
+     * @ORM\Column(name="publish_in_edugain", type="boolean")
      */
-    public $publishInEdugain = false;
+    public $publishInEdugain;
 
     /**
      * @var X509Certificate[]
+     *
+     * @ORM\Column(name="certificates", type="array")
      */
     public $certificates = array();
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="workflow_state", type="string")
      */
     public $workflowState = self::WORKFLOW_STATE_DEFAULT;
 
     /**
      * @var ContactPerson[]
+     *
+     * @ORM\Column(name="contact_persons", type="array")
      */
-    public $contactPersons = array();
+    public $contactPersons;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="name_id_format", type="string")
      */
-    public $nameIdFormat = null;
+    public $nameIdFormat;
 
     /**
      * @var string[]
+     *
+     * @ORM\Column(name="name_id_formats", type="array")
      */
-    public $nameIdFormats = array(
-        SAML2_Const::NAMEID_TRANSIENT,
-        SAML2_Const::NAMEID_PERSISTENT,
-    );
+    public $nameIdFormats;
 
     /**
-     * @var Service[]
+     * @var Service
      */
-    public $singleLogoutServices = array();
+    public $singleLogoutService;
 
     /**
-     * @var \DateTime
+     * @var DateTime
+     *
+     * @ORM\Column(name="publish_in_edu_gain_date", type="date")
      */
-    public $publishInEduGainDate = null;
-
-    /**
-     * @var bool
-     */
-    public $disableScoping = false;
+    public $publishInEduGainDate;
 
     /**
      * @var bool
+     *
+     * @ORM\Column(name="disable_scoping", type="boolean")
+     */
+    public $disableScoping;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="additional_logging", type="boolean")
      */
     public $additionalLogging = false;
 
     /**
      * @var bool
+     *
+     * @ORM\Column(name="requests_must_be_signed", type="boolean")
      */
     public $requestsMustBeSigned = false;
 
     /**
-     * @var Service
+     * @var string
+     *
+     * @ORM\Column(name="response_processing_service_binding", type="string")
      */
-    public $responseProcessingService = null;
+    public $responseProcessingService;
 
-    /**
-     * @param $entityId
-     */
-    public function __construct($entityId)
-    {
+    public function __construct(
+        $entityId,
+        Organization $organizationEn = null,
+        Organization $organizationNl = null,
+        Service $singleLogoutService = null,
+        $additionalLogging = false,
+        array $certificates = array(),
+        array $contactPersons = array(),
+        $descriptionEn = '',
+        $descriptionNl = '',
+        $disableScoping = false,
+        $displayNameEn = '',
+        $displayNameNl = '',
+        $keywordsEn = '',
+        $keywordsNl = '',
+        Logo $logo = null,
+        $nameEn = '',
+        $nameNl = '',
+        $nameIdFormat = null,
+        $nameIdFormats = array(
+            SAML2_Const::NAMEID_TRANSIENT,
+            SAML2_Const::NAMEID_PERSISTENT,
+        ),
+        $publishInEduGainDate = null,
+        $publishInEdugain = false,
+        $requestsMustBeSigned = false,
+        Service $responseProcessingService = null,
+        $workflowState = self::WORKFLOW_STATE_DEFAULT
+    ) {
+        $this->additionalLogging = $additionalLogging;
+        $this->certificates = $certificates;
+        $this->contactPersons = $contactPersons;
+        $this->descriptionEn = $descriptionEn;
+        $this->descriptionNl = $descriptionNl;
+        $this->disableScoping = $disableScoping;
+        $this->displayNameEn = $displayNameEn;
+        $this->displayNameNl = $displayNameNl;
         $this->entityId = $entityId;
+        $this->keywordsEn = $keywordsEn;
+        $this->keywordsNl = $keywordsNl;
+        $this->logo = $logo;
+        $this->nameEn = $nameEn;
+        $this->nameIdFormat = $nameIdFormat;
+        $this->nameIdFormats = $nameIdFormats;
+        $this->nameNl = $nameNl;
+        $this->organizationEn = $organizationEn;
+        $this->organizationNl = $organizationNl;
+        $this->publishInEduGainDate = $publishInEduGainDate;
+        $this->publishInEdugain = $publishInEdugain;
+        $this->requestsMustBeSigned = $requestsMustBeSigned;
+        $this->responseProcessingService = $responseProcessingService;
+        $this->singleLogoutService = $singleLogoutService;
+        $this->workflowState = $workflowState;
     }
 }
--- src/Entity/ServiceProvider.php
+++ src/Entity/ServiceProvider.php
@@ -3,62 +3,171 @@
 namespace OpenConext\Component\EngineBlockMetadata\Entity;
 
 use OpenConext\Component\EngineBlockMetadata\AttributeReleasePolicy;
+use OpenConext\Component\EngineBlockMetadata\Logo;
+use OpenConext\Component\EngineBlockMetadata\Organization;
 use OpenConext\Component\EngineBlockMetadata\RequestedAttribute;
 use OpenConext\Component\EngineBlockMetadata\IndexedService;
+use OpenConext\Component\EngineBlockMetadata\Service;
+use SAML2_Const;
 
 /**
  * Class ServiceProvider
  * @package OpenConext\Component\EngineBlockMetadata\Entity
  */
-class ServiceProvider extends AbstractConfigurationEntity
+class ServiceProvider extends AbstractRole
 {
     /**
      * @var IndexedService[]
+     *
+     * @ORM\Column(name="assertion_consumer_services", type="array")
      */
     public $assertionConsumerServices = array();
 
     /**
      * @var bool
+     *
+     * @ORM\Column(name="is_transparent_issuer", type="boolean")
      */
     public $isTransparentIssuer = false;
 
     /**
      * @var bool
+     *
+     * @ORM\Column(name="is_trusted_proxy", type="boolean")
      */
     public $isTrustedProxy = false;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="implicit_vo_id", type="string")
      */
     public $implicitVoId = null;
 
     /**
      * @var bool
+     *
+     * @ORM\Column(name="display_unconnected_idps_wayf", type="boolean")
      */
     public $displayUnconnectedIdpsWayf = false;
 
     /**
      * @var bool
+     *
+     * @ORM\Column(name="is_consent_required", type="boolean")
      */
     public $isConsentRequired = true;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="eula", type="string")
      */
     public $eula = null;
 
     /**
      * @var bool
+     *
+     * @ORM\Column(name="skip_denormalization", type="boolean")
      */
     public $skipDenormalization = false;
 
     /**
      * @var array
+     *
+     * @ORM\Column(name="allowed_idp_entity_ids", type="array")
      */
     public $allowedIdpEntityIds = array();
 
     /**
      * @var null|RequestedAttribute[]
+     *
+     * @ORM\Column(name="requested_attributes", type="array")
      */
     public $requestedAttributes = null;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="manipulation", type="text")
+     */
+    protected $manipulation;
+
+    public function __construct(
+        $entityId,
+        Organization $organizationEn = null,
+        Organization $organizationNl = null,
+        Service $singleLogoutService = null,
+        $additionalLogging = false,
+        array $certificates = array(),
+        array $contactPersons = array(),
+        $descriptionEn = '',
+        $descriptionNl = '',
+        $disableScoping = false,
+        $displayNameEn = '',
+        $displayNameNl = '',
+        $keywordsEn = '',
+        $keywordsNl = '',
+        Logo $logo = null,
+        $nameEn = '',
+        $nameNl = '',
+        $nameIdFormat = null,
+        $nameIdFormats = array(
+            SAML2_Const::NAMEID_TRANSIENT,
+            SAML2_Const::NAMEID_PERSISTENT,
+        ),
+        $publishInEduGainDate = null,
+        $publishInEdugain = false,
+        $requestsMustBeSigned = false,
+        Service $responseProcessingService = null,
+        $workflowState = self::WORKFLOW_STATE_DEFAULT,
+        array $allowedIdpEntityIds = array(),
+        array $assertionConsumerServices = array(),
+        $displayUnconnectedIdpsWayf = false,
+        $eula = null,
+        $implicitVoId = null,
+        $isConsentRequired = true,
+        $isTransparentIssuer = false,
+        $isTrustedProxy = false,
+        $requestedAttributes = null,
+        $skipDenormalization = false
+    ) {
+        parent::__construct(
+            $entityId,
+            $organizationEn,
+            $organizationNl,
+            $singleLogoutService,
+            $additionalLogging,
+            $certificates,
+            $contactPersons,
+            $descriptionEn,
+            $descriptionNl,
+            $disableScoping,
+            $displayNameEn,
+            $displayNameNl,
+            $keywordsEn,
+            $keywordsNl,
+            $logo,
+            $nameEn,
+            $nameNl,
+            $nameIdFormat,
+            $nameIdFormats,
+            $publishInEduGainDate,
+            $publishInEdugain,
+            $requestsMustBeSigned,
+            $responseProcessingService,
+            $workflowState
+        );
+
+        $this->allowedIdpEntityIds = $allowedIdpEntityIds;
+        $this->assertionConsumerServices = $assertionConsumerServices;
+        $this->displayUnconnectedIdpsWayf = $displayUnconnectedIdpsWayf;
+        $this->eula = $eula;
+        $this->implicitVoId = $implicitVoId;
+        $this->isConsentRequired = $isConsentRequired;
+        $this->isTransparentIssuer = $isTransparentIssuer;
+        $this->isTrustedProxy = $isTrustedProxy;
+        $this->requestedAttributes = $requestedAttributes;
+        $this->skipDenormalization = $skipDenormalization;
+    }
 }
