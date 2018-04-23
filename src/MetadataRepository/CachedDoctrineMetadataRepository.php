3b50a4656898a505d742e4ca0a5aa9943b17fe6c
Joris Steyn <joris@ibuildings.nl>
2018-04-23 16:34:34 +0200

    Do not show all IDPs in WAYF when using experimental caching
    See:
     - https://www.pivotaltracker.com/n/projects/1425900/stories/156995448
     - https://github.com/OpenConext/OpenConext-engineblock/pull/524

--- src/MetadataRepository/CachedDoctrineMetadataRepository.php
+++ src/MetadataRepository/CachedDoctrineMetadataRepository.php
@@ -82,6 +82,8 @@ class CachedDoctrineMetadataRepository implements MetadataRepositoryInterface
     {
         $this->repository->appendFilter($filter);
 
+        $this->clearResultsCache();
+
         return $this;
     }
 
@@ -93,7 +95,23 @@ class CachedDoctrineMetadataRepository implements MetadataRepositoryInterface
     {
         $this->repository->appendVisitor($visitor);
 
+        $this->clearResultsCache();
+
         return $this;
+    }
+
+    /**
+     * Reset the results cache.
+     *
+     * The cache is only valid for a specific combination of filters and
+     * visitors. If a filter or visitor is appended, the previously cached
+     * results are discarded by calling this method. In practice,
+     * visitor/filter setup is only done in the beginning of the request so
+     * resetting the cache has little impact on the total number of queries.
+     */
+    private function clearResultsCache()
+    {
+        $this->cache = array();
     }
 
     /**
